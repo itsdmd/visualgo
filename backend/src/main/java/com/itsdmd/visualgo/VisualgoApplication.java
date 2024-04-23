7bce828b006799efaf62cffd7cfcfe93707e7f9a
Đức Đào <duc.dao.431@gmail.com>
2024-04-24 00:03:54 +0700

    Initial commit

---
+++ backend/src/main/java/com/itsdmd/visualgo/VisualgoApplication.java
@@ -0,0 +1,13 @@
+package com.itsdmd.visualgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualgoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/itsdmd/visualgo/VisualgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsdmd.visualgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
